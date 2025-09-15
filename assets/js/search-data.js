ef16dc093033c2cccdf58ac4213530dbcdc24ef6
joneswong <joneswong@users.noreply.github.com>
2025-09-15 09:54:49 +0000

    deploy: joneswong/pw-src@db7c2bf48008cf47db55da0fd9772babfbe473a6

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%6F%6E%65%73%77%6F%6E%67.%6D%6C@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -522,7 +522,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=e5CqTBMAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
